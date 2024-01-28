b2b424c70ae0d641e3b61da4e25754f1b951c5f8
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-01-28 18:35:15 +0900

    collection for, maps, sets

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # study-dart
  
+노션 링크 -> https://dandelion-echidna-7a3.notion.site/Dart-6c6864c12f394470b598d4b8601a2e99
---
+++ nico/Maps.dart
@@ -0,0 +1,23 @@
+void main () {
+  // var player = {
+  //   'name' : 'nico',
+  //   'xp': 19.99,
+  //   'superpower' : false
+  // };
+
+  // Map<int, bool> player = {
+  //   1 : true,
+  //   2 : false,
+  // };
+  
+  // Map<List<int>, bool> player = {
+  //   [1, 2, 3, 4] : false,
+  // };
+
+  List<Map<String, Object>> player = [
+    {'name' : 'nico', 'xp' : 19999.20},
+    {'name' : 'nico', 'xp' : 19919.20}
+  ];
+
+  print(player);
+}
