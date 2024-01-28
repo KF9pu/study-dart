6ec04c6f5828180503458496e559138ced4df431
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-01-28 18:39:41 +0900

    function

---
+++ nico/functions.dart
@@ -0,0 +1,17 @@
+// void sayHello(String name) {
+//   print("Hello $name nice to meet you!");
+// }
+
+// String sayHello(String name) => ("Hello $name nice to meet you!");
+
+String sayHello(String name) {
+  // call api
+  // rest code
+  return ("Hello $name nice to meet you!");
+}
+
+num plus(num a, num b) => a + b;
+
+void main() {
+  print(sayHello('nico'));
+}
