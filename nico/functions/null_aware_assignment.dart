ccc0051107003a5fea85cbac60ce9c78e6918092
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-01-29 17:12:28 +0900

    null aware assignment 주석 추가

--- nico/functions/null_aware_assignment.dart
+++ nico/functions/null_aware_assignment.dart
@@ -1,6 +1,6 @@
 void main() {
   String? name;
-  name ??= 'nico';
+  name ??= 'nico'; // 초기 할당 값이 null 이기 때문에 'nico' 할당
   name = null;
-  name ??= 'another';
+  name ??= 'another'; // name 이 null 로 변경되었기 때문에 'another' 할당
 }
