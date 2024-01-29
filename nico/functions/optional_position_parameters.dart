e4beb07ead8e20feaba1feae5044dfa28194a2e1
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-01-29 15:55:16 +0900

    optional position parameters

--- nico/functions/optional_position_parameters.dart
+++ nico/functions/optional_position_parameters.dart
@@ -1,3 +1,10 @@
+String sayHello(
+  String name, 
+  int age, 
+  [String? country = "cuba"]
+) => "Hello $name, you are $age, and you come from $country";
+
+
 void main() {
-  
+  sayHello('d', 12);
 }
