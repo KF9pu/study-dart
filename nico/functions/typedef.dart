eb1b05c883afa32e168f6aea7e2cffc7a4b8385b
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-01-29 17:21:17 +0900

    typedef

--- nico/functions/typedef.dart
+++ nico/functions/typedef.dart
@@ -1,3 +1,20 @@
+// typedef ListOfInts = List<int>;
+
+// List<int> reverseListOfNumbers(ListOfInts list) {
+//   var reversed = list.reversed; // revesed 메소드를 사용할 경우 타입이 Iterable<int>가 된다.
+//   return reversed.toList(); // Iterable 타입을 list 로 다시 변환하여 리턴한다.
+// }
+
+// void main() {
+//   reverseListOfNumbers([1,2,3]);
+// }
+
+typedef UserInfo = Map<String, String>;
+
+String sayHi(UserInfo userInfo) {
+  return "Hi ${userInfo['name']}";
+}
+
 void main() {
-  
+  sayHi({"name" : "hongs"});
 }
