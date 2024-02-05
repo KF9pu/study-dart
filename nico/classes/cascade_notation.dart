3a07faac555e0af5f836e1e6a57e39323209b57f
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-02-05 20:54:34 +0900

    cascade notation

---
+++ nico/classes/cascade_notation.dart
@@ -0,0 +1,24 @@
+class Player {
+  String name;
+  int xp;
+  String team;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
+  void sayHello(){
+    print("Hi my name is $name");
+  }
+}
+
+void main() {
+  var nico = Player(name: "nico", xp: 1200, team: 'red');
+  var potato = nico
+  ..name = 'las'
+  ..xp = 120000
+  ..team = 'blue'
+  ..sayHello();
+}
