36f045225bb487527f7c74f1bee1fe3d913b862f
hong seong hwa <60565249+KF9pu@users.noreply.github.com>
2024-02-05 20:50:48 +0900

    constructor recap

--- nico/classes/constructors.dart
+++ nico/classes/constructors.dart
@@ -1,20 +1,51 @@
-class Player {
-  String name;
-  int xp;
+// class Player {
+//   String name;
+//   int xp;
 
-  // Player(String name, int xp) {
-  //   this.name = name;
-  //   this.xp = xp;
-  // }
+//   // Player(String name, int xp) {
+//   //   this.name = name;
+//   //   this.xp = xp;
+//   // }
 
-  // Player(this.name, this.xp);
+//   Player(this.name, this.xp);
+// }
 
-  Player.hello(this.name, this.xp){
-    print("Start Player_hello constructor");
+// void main() {
+//   var player = Player('nico', 1500);
+//   var player2 = Player('lynn', 2500);
+// }
+
+// --------------------------------------
+
+// class Player {
+//   String name;
+//   int xp;
+
+//   Player.hello(this.name, this.xp){
+//     print("Start Player_hello constructor");
+//   }
+// }
+
+// void main() {
+//   var player2 = Player.hello('lynn', 2500);
+// }
+
+// --------------------------------------
+
+class Logger {
+  static final Map<String, Logger> _cache = {};
+
+  factory Logger(String name) {
+    if (_cache.containsKey(name)) {
+      return _cache[name]!;
+    } else {
+      final logger = Logger._internal(name);
+      _cache[name] = logger;
+      return logger;
+    }
   }
-}
 
-void main() {
-  // var player = Player('nico', 1500);
-  var player2 = Player.hello('lynn', 2500);
+  Logger._internal(this.name);
+
+  final String name;
 }
